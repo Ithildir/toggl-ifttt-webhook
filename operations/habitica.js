edf8fd230c54b93eb6973b6f2570951482707a4c
Andrea Di Giorgi <andrea.digiorgi@joinhoney.com>
2020-05-03 12:28:56 -0700

    Extract Habitica headers for reuse

--- operations/habitica.js
+++ operations/habitica.js
@@ -3,14 +3,16 @@ const habiticaKeywords = require('./habiticaKeywords.json');
 
 const { HABITICA_API_KEY, HABITICA_USER_ID } = process.env;
 
+const HABITICA_HEADERS = {
+  'x-api-key': HABITICA_API_KEY,
+  'x-api-user': HABITICA_USER_ID,
+  'x-client': `${HABITICA_USER_ID}-Testing`,
+};
+
 async function score({ direction, taskId }) {
   await request
     .post(`https://habitica.com/api/v3/tasks/${taskId}/score/${direction}`)
-    .set({
-      'x-api-key': HABITICA_API_KEY,
-      'x-api-user': HABITICA_USER_ID,
-      'x-client': `${HABITICA_USER_ID}-Testing`,
-    });
+    .set(HABITICA_HEADERS);
 
   console.log(`Scored Habitica task ${taskId} ${direction}`);
 }
