3268dcfb5918a5de69c5f799b322e6d551f537c5
Andrea Di Giorgi <andrea.digiorgi@joinhoney.com>
2020-05-02 21:20:37 -0700

    Refactor Toggl logic into new "operations" dir

--- api/toggl.js
+++ api/toggl.js
@@ -1,48 +1,13 @@
-const TogglClient = require('toggl-api');
-const { promisify } = require('util');
+const toggl = require('../operations/toggl');
 const { sendStatus, withAuth } = require('../utils');
-
-const { TOGGL_API_TOKEN } = process.env;
-
-const toggl = new TogglClient({ apiToken: TOGGL_API_TOKEN });
-
-const getCurrentTimeEntryAsync = promisify(
-  toggl.getCurrentTimeEntry.bind(toggl)
-);
-const startTimeEntryAsync = promisify(toggl.startTimeEntry.bind(toggl));
-const stopTimeEntryAsync = promisify(toggl.stopTimeEntry.bind(toggl));
-
-async function stopTimer() {
-  const timeEntry = await getCurrentTimeEntryAsync();
-
-  if (timeEntry) {
-    await stopTimeEntryAsync(timeEntry.id);
-
-    console.log(
-      `Stopped time entry ${timeEntry.id}: "${timeEntry.description}"`
-    );
-  }
-}
-
-async function startTimer({ description }) {
-  await stopTimer();
-
-  const fixedDescription = description.trim().toLowerCase();
-
-  const timeEntry = await startTimeEntryAsync({
-    description: fixedDescription,
-  });
-
-  console.log(`Started time entry ${timeEntry.id}: "${timeEntry.description}"`);
-}
 
 module.exports = withAuth(async (req, res) => {
   const { cmd } = req.body;
 
   if (cmd === 'start') {
-    await startTimer(req.body);
+    await toggl.startTimer(req.body);
   } else if (cmd === 'stop') {
-    await stopTimer();
+    await toggl.stopTimer();
   } else {
     return sendStatus(res, 400, 'Wrong cmd');
   }
---
+++ operations/toggl.js
@@ -0,0 +1,41 @@
+const TogglClient = require('toggl-api');
+const { promisify } = require('util');
+
+const { TOGGL_API_TOKEN } = process.env;
+
+const toggl = new TogglClient({ apiToken: TOGGL_API_TOKEN });
+
+const getCurrentTimeEntryAsync = promisify(
+  toggl.getCurrentTimeEntry.bind(toggl)
+);
+const startTimeEntryAsync = promisify(toggl.startTimeEntry.bind(toggl));
+const stopTimeEntryAsync = promisify(toggl.stopTimeEntry.bind(toggl));
+
+async function stopTimer() {
+  const timeEntry = await getCurrentTimeEntryAsync();
+
+  if (timeEntry) {
+    await stopTimeEntryAsync(timeEntry.id);
+
+    console.log(
+      `Stopped time entry ${timeEntry.id}: "${timeEntry.description}"`
+    );
+  }
+}
+
+async function startTimer({ description }) {
+  await stopTimer();
+
+  const fixedDescription = description.trim().toLowerCase();
+
+  const timeEntry = await startTimeEntryAsync({
+    description: fixedDescription,
+  });
+
+  console.log(`Started time entry ${timeEntry.id}: "${timeEntry.description}"`);
+}
+
+module.exports = {
+  startTimer,
+  stopTimer,
+};
